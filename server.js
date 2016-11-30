f590039166084d6f9a1fcd98225319d167231cb5
ovarunendra <vpsingh016@gmail.com>
2016-11-30 09:36:25 +0530

    modifed server

--- server.js
+++ server.js
@@ -1,20 +1,16 @@
-var fs = require('fs');
-var fileUpload = require('express-fileupload');
-
 var express = require('express');
 var app = express();
-app.use(fileUpload());
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('running');
+  response.render('running');
+  response.end();
 });
 
-app.get('/getfile', function(req, res) {
-    fs.readFile(__dirname + '/test.js', "utf8", function(err, data) {
-        if (err) throw err;
-        //res.write(data);
-        res.jsonp({ user: data });
-        res.end();
-    });
-})
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
 
