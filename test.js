3d324962b2c4fe5c96ba417d489dc4b5c1bca9e4
ovarunendra <vpsingh016@gmail.com>
2017-01-12 08:01:13 +0530

    product images

--- test.js
+++ test.js
@@ -1,15 +1,21 @@
-scene.background = scene.addSprite("blippar_background.png")
-                            .setName("background")
-		                    .setScale(sw, sh, 1)
-		                    .setClickable(false);
-		                    
-scene.textMsg = scene.addText("Click to open overlayHTML")
-      .setTranslation(0, -360, 1)
-      .setScale(2)
-      .setColor('FF0000')
-      .setClickable(true);
+// Scene global variables
+scene.animDuration     = 5000;
+scene.intervalDuration = scene.animDuration + 500;
+scene.meshIndexStart   = 1;
+scene.meshIndexStop    = 100;
+scene.fighterSound     = "fighther_launch.mp3";
+scene.themeSong        = "theme_song.mp3";
+scene.tPathsList       = ["tpath_1.md2", "tpath_2.md2", "tpath_3.md2"];
+scene.tPathsMax		   = 3;
 
-scene.textMsg.onTouchEnd = function (id, x, y) {
-  console.log("called:"+id);
-};
+// Load additional ressources
+scene.setRequiredAssets(scene.tPathsList);
+scene.addRequiredAssets([scene.fighterSound, scene.themeSong]);
 
+// Get the scene objects
+scene.background = scene.getChild("background");
+scene.tpath      = scene.getChild("tpath");
+scene.tieFighter = scene.tpath.getChild("tieFighter");
+
+// Adjust background size
+scene.background.setScale(sW, sH, 1);
